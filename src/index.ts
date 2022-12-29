f93716f76cb93a7c99f94bee7575c136be664cd0
André Martins <andre.lucas.medeir@gmail.com>
2022-12-29 00:57:02 -0300

    feat: corrigido start e build, adicionado README, adicionado timer

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import { Console, publishAll } from './publishers'
 import { runScrapers } from './scrapers'
 
 // monday to friday at midnight
-// cron.schedule("0 0 0 * * 1,2,3,4,5", async () => {
-cron.schedule("*/10 * * * * *", async () => {
+cron.schedule("0 0 0 * * 1,2,3,4,5", async () => {
 
     let opportunities = await runScrapers()
 
